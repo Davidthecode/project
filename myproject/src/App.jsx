f48185bbb47397a091607a2834af7e842ef395bd
david <you@example.com>
2022-10-16 00:01:13 +0100

    project

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import { Link } from 'react-router-dom'
+import LoadingSpinner from './components/loadingSpinner'
 function App() {
   return (
     <div>
@@ -12,6 +13,7 @@ function App() {
         <span>click here to signup</span>
         <Link to='/signup'>signup</Link>
       </div>
+      <LoadingSpinner />
     </div>
   )
 }
