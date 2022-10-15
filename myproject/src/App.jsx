964c99d5dce8df245541a181c2e37a8ee262e7cd
david <you@example.com>
2022-10-16 00:15:51 +0100

    project

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { Link } from 'react-router-dom'
 import LoadingSpinner from './components/loadingSpinner'
+
 function App() {
+  const [loading, setLoading] = useState(false)
+  const handleEvent = ()=>{
+    setLoading(true)
+
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+    
+  }
   return (
     <div>
-      <div>
+      <div disabled={loading}>
         <span>click here to login</span>
-        <Link to='/login'>login</Link>
+        <Link to='/login' onClick={handleEvent}>login</Link>
       </div>
-      <div>
+      <div disabled={loading}>
         <span>click here to signup</span>
         <Link to='/signup'>signup</Link>
       </div>
